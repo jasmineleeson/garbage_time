e7d95bd7844f689ab3a211a9d47f182e65f4bd4c
Jasmine <jasmine.leeson@gmail.com>
2016-12-29 16:02:56 -0500

    change mailing host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'arcane-scrubland-85798.herokuapp.com'
+  host = 'rocky-depths-47188.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
